d0d6338bd71c90a6d512fa608ec6e2cff23c84ed
Theodore Quinn <tcquinn@tcquinn.org>
2020-09-15 14:28:31 -0500

    Fix up endpoint for Fibonacci test

--- rpi_monitor/workers.py
+++ rpi_monitor/workers.py
@@ -16,15 +16,25 @@ def log_rpi_status(
 
 @click.command()
 @click.option('-n', type=int, default=10000, show_default=True, help='Fibonacci number to repeatedly calculate')
+@click.option('-i', '--iterations', type=int, default=1000, show_default=True, help='Number of times the program calculates this number in each batch')
+@click.option('--parallel/--no-parallel', default=True, help='Turn parallel processing on/off [default:  off]')
 @click.option('--num_processes', type=int, help='Number of parallel processes to launch [default: number of cores]')
+@click.option('--log-level', help='Log level (e.g., warning, info, debug, etc.)')
 def fibonacci_test(
     n,
-    num_processes
+    iterations,
+    parallel,
+    num_processes,
+    log_level
 ):
     rpi_monitor.tests.fibonacci_test(
         n=n,
-        num_processes=num_processes
+        iterations=iterations,
+        parallel=parallel,
+        num_processes=num_processes,
+        log_level=log_level
     )
+
 
 @click.command()
 @click.option('-d', '--delay', type=int, default=5, show_default=True, help='Number of seconds to wait before returning')
